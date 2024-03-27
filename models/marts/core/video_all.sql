8fe6bf47408dfc63213c27b71376492c1bdb961d
hh8@nimbusintelligence.com <hh8@nimbusintelligence.com>
2024-03-27 14:30:59 +0000

    Some updates

--- models/marts/core/video_all.sql
+++ models/marts/core/video_all.sql
@@ -7,11 +7,6 @@ WITH video_info as (
         , channel_title
         , comments_disabled
         , description
-        , garbled_1
-        , garbled_2
-        , garbled_3
-        , garbled_4
-        , garbled_5
         , published_at
         , ratings_disabled
         , tags
@@ -26,7 +21,7 @@ comments as (
         date_file
         , comment_count
         , video_id
-        ,country_code
+        , country_code
     FROM {{ ref('stg_comment_count') }}
 ), 
 likes as (
@@ -45,20 +40,30 @@ views as (
         , country_code
     FROM {{ ref('stg_view_count') }}
 ),
+
+countries as (
+    select
+        country_code
+        , country_name
+    from {{ ref('countries') }}
+),
+
+categories as (
+    select 
+        category_id
+        , category_title
+    from {{ ref('video_categories') }}
+),
+
 all_info as (
     SELECT 
         video_info.date_file
         , video_info.video_id
-        , category_id
+        , categories.category_title
         , channel_id
         , channel_title
         , comments_disabled
         , description
-        , garbled_1
-        , garbled_2
-        , garbled_3
-        , garbled_4
-        , garbled_5
         , published_at
         , ratings_disabled
         , tags
@@ -68,10 +73,24 @@ all_info as (
         , comment_count
         , likes
         , view_count
+        , countries.country_name
     FROM video_info
-    LEFT JOIN comments ON video_info.video_id = comments.video_id AND video_info.date_file = comments.date_file AND video_info.country_code = comments.country_code
-    LEFT JOIN likes ON video_info.video_id = likes.video_id AND video_info.date_file = likes.date_file AND video_info.country_code = likes.country_code
-    LEFT JOIN views ON video_info.video_id = views.video_id AND video_info.date_file = views.date_file AND video_info.country_code = views.country_code
+    LEFT JOIN comments 
+        ON video_info.video_id = comments.video_id 
+        AND video_info.date_file = comments.date_file 
+        AND video_info.country_code = comments.country_code
+    LEFT JOIN likes 
+        ON video_info.video_id = likes.video_id 
+        AND video_info.date_file = likes.date_file 
+        AND video_info.country_code = likes.country_code
+    LEFT JOIN views 
+        ON video_info.video_id = views.video_id 
+        AND video_info.date_file = views.date_file 
+        AND video_info.country_code = views.country_code
+    LEFT JOIN countries
+        ON video_info.country_code = countries.country_code
+    left join categories
+        on video_info.category_id = categories.category_id
 )
 
 SELECT * FROM all_info
