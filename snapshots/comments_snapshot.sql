c1dcda85f82c52e9a3dbf8d8834b3e69279b77ca
hypehouse8 <hh8@nimbusintelligence.com>
2024-04-03 13:50:20 +0200

    New snapshot...

---
+++ snapshots/comments_snapshot.sql
@@ -0,0 +1,16 @@
+{% snapshot comments_snapshot %}
+
+{{
+    config(
+      target_database='dev',
+      target_schema='snapshots',
+      unique_key='id',
+
+      strategy='timestamp',
+      updated_at='date_file',
+    )
+}}
+
+select video_id||'-'||country_code as id, * from {{ ref('stg_comment_count') }}
+
+{% endsnapshot %}
